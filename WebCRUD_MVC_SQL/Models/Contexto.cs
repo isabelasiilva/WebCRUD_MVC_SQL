03dd79058438ab3ff69015419c69092a2ca0252c
Isabela <silvaisabela13@hotmail.com>
2025-04-09 01:15:12 -0300

    Adicionar arquivos de projeto.

---
+++ WebCRUD_MVC_SQL/Models/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebCRUD_MVC_SQL.Models
+{
+    public class Contexto : DbContext
+    { 
+        // criando o construtor do Contexto
+        public Contexto(DbContextOptions<Contexto> options) : base(options) 
+        { 
+
+        }
+        // Definindo quais sao os modelos que devem ser gerenciados pelo migrations
+        public DbSet<Produto> Produto { get; set; }
+    }
+}
